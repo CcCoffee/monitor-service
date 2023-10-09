61c087bad4b07f9b80be575674ff5c7cfc411261
Kevin K H Zheng <kevin.k.h.zheng@qq.com>
2023-10-10 00:22:36 +0800

    beatify page

--- monitor-center/ui/src/pages/AgentStatus.jsx
+++ monitor-center/ui/src/pages/AgentStatus.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container, Row, Col, Spinner } from 'react-bootstrap';
+import { Container, Row, Col, Spinner, Card } from 'react-bootstrap';
 import HealthCard from '../components/HealthCard';
 
 function AgentStatus() {
@@ -21,27 +21,30 @@ function AgentStatus() {
   }, []);
 
   return (
-    <Container fluid className='h-100'>
-      {loading ? (
-        <div className='d-flex justify-content-center align-items-center h-100'>
-          <Spinner animation='border' role='status'>
-            <span className='visually-hidden'>Loading...</span>
-          </Spinner>
-        </div>
-      ) : (
-        <>
-        <Row>
-          <Col><h3>Agent health status</h3></Col>
-        </Row>
-        <Row className='g-2'>
-          {agents.map((agent) => (
-            <Col key={agent.hostname} xs={12} sm={6} md={4} lg={3}>
-              <HealthCard hostname={agent.hostname} status={agent.status} />
-            </Col>
-          ))}
-        </Row>
-        </>
-      )}
+    <Container className='h-100 w-100 py-2'>
+      <Card  className='h-100 w-100'>
+        <Card.Body>
+          <Card.Title>Agent health status</Card.Title>
+          <hr/>
+          <Container fluid>
+        {loading ? (
+          <div className='d-flex justify-content-center align-items-center h-100'>
+            <Spinner animation='border' role='status'>
+              <span className='visually-hidden'>Loading...</span>
+            </Spinner>
+          </div>
+        ) : (
+          <Row className='g-2'>
+            {agents.map((agent) => (
+              <Col key={agent.hostname} xs={12} sm={6} md={4} lg={3}>
+                <HealthCard hostname={agent.hostname} status={agent.status} />
+              </Col>
+            ))}
+          </Row>
+        )}
+      </Container>
+        </Card.Body>
+      </Card>
     </Container>
   );
 }
